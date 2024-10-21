fdb28143f9a2df101e5b6ee5b9fc8c04ae0b30ec
Wesley Batista Alcantara <wesleydevadm@gmail.com>
2024-10-21 09:52:12 -0300

    Update server.js

--- server.js
+++ server.js
@@ -2,19 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Middleware para interpretar dados de formulários
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public')); // Supondo que seus arquivos estão na pasta 'public'
+app.use(express.static('public'));
 
 // Endpoint para processar o envio do formulário
 app.post('/lead', (req, res) => {
     const { nome, email, telefone } = req.body;
     console.log('Dados recebidos:', { nome, email, telefone });
     
-    // Aqui você pode adicionar lógica para salvar os dados no Firestore se desejar.
-    
-    res.status(200).send('Dados recebidos com sucesso'); // Resposta para o cliente
+    res.status(200).send('Dados recebidos com sucesso');
 });
 
 app.listen(port, () => {
